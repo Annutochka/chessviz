33d9ab0acec8fe2647635977ba06131f9a3a2fed
Neytral100 <rymarev.maksim@mail.ru>
2018-02-21 19:19:46 +0700

    create chess desk program

---
+++ desk.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+printf("  a b c d e f g h\n");
+printf("1 L N O Q K O N L\n");
+printf("2 P P P P P P P P\n");
+printf("3\n");
+printf("4\n");
+printf("5\n");
+printf("6\n");
+printf("7 p p p p p p p p\n");
+printf("8 l n o q k o n l\n");
+return 0;
+}
